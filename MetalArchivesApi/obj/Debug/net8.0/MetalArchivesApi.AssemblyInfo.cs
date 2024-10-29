a27462ba7ada9d036b2e35be6a352dc390d5bd6b
Michele Segata <michele.segata@zupit.it>
2024-10-29 17:06:31 +0100

    feat(MAPI-2): fix connection string

--- MetalArchivesApi/obj/Debug/net8.0/MetalArchivesApi.AssemblyInfo.cs
+++ MetalArchivesApi/obj/Debug/net8.0/MetalArchivesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MetalArchivesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa433564e90d8ceb8a96f1d6e4aa883de33ee365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5730f63eb42f2d20c18e24ce056c3c7f76b36ea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MetalArchivesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MetalArchivesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
